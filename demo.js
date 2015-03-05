a3bf4c86ebc6430235d0f58ad8028cab1ac9fc26
Tom Shea <tom@shea.at>
2015-03-05 01:48:41 +0000

    Run apps after desktop element appended

--- demo.js
+++ demo.js
@@ -4,6 +4,8 @@ var Desktopical = require('../desktopical');
 var Desktop = new Desktopical();
 Desktop.registerApplication(require('./apps/demoApp'));
 Desktop.registerApplication(require('./apps/faceApp.js'));
+
+document.body.appendChild(Desktop.element);
+
 Desktop.run("demo");
 Desktop.run("face");
-document.body.appendChild(Desktop.element);
