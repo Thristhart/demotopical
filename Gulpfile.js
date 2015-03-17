8f4df35d008cf37e3ef97e0052b975c2be0b2809
Tom Shea <tom@shea.at>
2015-03-17 22:16:13 +0000

    Reorganize gulpfile and add menuine dependency

--- Gulpfile.js
+++ Gulpfile.js
@@ -16,6 +16,7 @@ var paths = {
   windowsill_dev_static: "../windowsill/**/*.css",
   desktopical_dev_js: "../desktopical/**/*.js",
   desktopical_dev_static: "../desktopical/**/*.css",
+  menuine_dev_js: "../menuine/**/*.js",
 
   deploy: "/srv/http/tom.shea.at/desktopical-demo/"
 };
@@ -27,7 +28,10 @@ function onError(err) {
 
 gulp.task('browserify', function() {
   var browserifyTransform = transform(function(filename) {
-    var b = browserify({entries: filename, debug:true});
+    var b = browserify({
+      entries: filename,
+      debug: true
+    });
     return b.bundle();
   });
 
@@ -41,12 +45,16 @@ gulp.task('browserify', function() {
 });
 
 gulp.task('watch', function() {
-  gulp.watch([paths.index, paths.apps, paths.windowsill_dev_js, paths.desktopical_dev_js], ['deploy-js']);
-  gulp.watch([paths.html, paths.stylesheet, paths.windowsill_dev_static, paths.desktopical_dev_static], ['deploy-static']);
+  gulp.watch([paths.index, paths.apps,
+              paths.windowsill_dev_js, paths.desktopical_dev_js, paths.menuine_dev_js],
+             ['deploy-js']);
+  gulp.watch([paths.html, paths.stylesheet,
+              paths.windowsill_dev_static, paths.desktopical_dev_static],
+             ['deploy-static']);
 });
 
 gulp.task('deploy-js', ['browserify'], function() {
-  gulp.src('./build/*')
+  gulp.src(['./build/*'])
     .pipe(gulp.dest(paths.deploy));
 });
 
@@ -58,6 +66,10 @@ gulp.task('deploy-static', function() {
     .pipe(gulp.dest(paths.deploy));
 });
 
+gulp.task('clean', function(callback) {
+  del(['./build'], callback);
+});
+
 gulp.task('deploy', ['deploy-js', 'deploy-static']);
 
-gulp.task('default', ['browserify']);
+gulp.task('default', ['browserify', 'browserify-dependencies']);
